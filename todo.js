26dd5cff5e5e0df8fc6c120e6a0177be373456fc
unknown <felipeAgomes>
2020-03-21 15:53:09 -0300

    Salvando no Storange

--- todo.js
+++ todo.js
@@ -2,16 +2,10 @@ var listElement = document.querySelector('#app ul');
 var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
 
-var todos = [
-    'Fazer café',
-    'Estudar JavaScript',
-    'Acessar comunidade da rocketseat'
-];
+var todos = JSON.parse(localStorage.getItem('list_todos')) || [];
 
 function renderTodos() {
     listElement.innerHTML = '';
-
-
     for(todo of todos){
         var todoElement = document.createElement('li');
         var todoText = document.createTextNode(todo);
@@ -36,12 +30,12 @@ function renderTodos() {
 
 renderTodos();
 
-function addTodo() {
+function addTodo(){
     var todoText = inputElement.value;
-
     todos.push(todoText);
     inputElement.value = '';
     renderTodos();
+    saveToStorage();
 };
 
 buttonElement.onclick = addTodo;
@@ -50,4 +44,10 @@ buttonElement.onclick = addTodo;
 function deleteTodo(pos) {
     todos.splice(pos, 1);
     renderTodos();
+    saveToStorage();
+};
+
+
+function saveToStorage(){
+    localStorage.setItem('list_todos', JSON.stringify(todos));
 };
